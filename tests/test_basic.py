177e17cdcbebed6f36334051c4aa9bee71522784
Adam <adamcrawford253@gmail.com>
2023-07-25 14:30:06 -0700

    trying to get this to work

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -1,11 +1,11 @@
-import ee
-from ee import ImageCollection
-import pytest
+# import ee
+# from ee import ImageCollection
+# import pytest
 
 # we should change the name of our Image class - it conflicts with the class image in the ee package, and people will
 # likely be using both. Let's not cause confusion
 
-import eedl
+# import eedl
 
 
 def test_int_equality():
