ca67d62b10ece347a8eccae2ff0734800c0598ad
Adam <adamcrawford253@gmail.com>
2023-08-04 13:51:28 -0700

    Running tests

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -5,3 +5,40 @@
 
 # we should change the name of our Image class - it conflicts with the class image in the ee package, and people will
 # likely be using both. Let's not cause confusion
+def test_int_equality():
+    assert 1 == 1
+    assert 2 == 2
+    assert 3 == 3
+    assert 4 == 4
+    assert 5 == 5
+    assert 6 == 6
+    assert 7 == 7
+    assert 8 == 8
+    assert 9 == 9
+    assert 10 == 10
+
+
+def test_int_equality2():
+    assert 1 == 1
+    assert 2 == 2
+    assert 3 == 3
+    assert 4 == 4
+    assert 5 == 5
+    assert 6 == 6
+    assert 7 == 7
+    assert 8 == 8
+    assert 9 == 9
+    assert 10 == 10
+
+
+def test_int_equality3():
+    assert 1 == 1
+    assert 2 == 2
+    assert 3 == 3
+    assert 4 == 4
+    assert 5 == 5
+    assert 6 == 6
+    assert 7 == 7
+    assert 8 == 8
+    assert 9 == 9
+    assert 10 == 10
